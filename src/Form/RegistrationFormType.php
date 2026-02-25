29c5385357fe67864936e7c15ea889e7bd28cdfb
Reyan Ghazzaoui <ghazzaoui.reyan@gmail.com>
2026-02-25 17:47:40 +0100

    Fin du back de la page de connexion et d'inscription, il ne manque plus que le front

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,12 +4,12 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -31,12 +31,14 @@ class RegistrationFormType extends AbstractType
                     ),
                 ],
             ])
-            ->add('mail')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
+            ->add('mail', EmailType::class, [
+                'label' => 'Adresse e-mail',
                 'constraints' => [
-                    new IsTrue(
-                        message: 'You should agree to our terms.',
+                    new NotBlank(
+                        message: 'Veuillez entrez votre adresse e-mail',
+                    ),
+                    new Email(
+                        message: 'Le format du mail est invalide',
                     ),
                 ],
             ])
